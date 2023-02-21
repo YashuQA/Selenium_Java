28b4ec01b7393059395d5995d0feb20ec960b3c0
YashuQA <yaseswiniy@duck.com>
2023-02-21 14:28:18 +0530

    Taking Screenshot Code

---
+++ src/test/java/Advanced/TakingScreenShot.java
@@ -0,0 +1,33 @@
+package Advanced;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TakingScreenShot {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demoqa.com");
+		
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(screenshot,new File("D:\\QAWorkSpace\\Selenium_JAVA\\resources\\Screenshots\\image.png"));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		driver.close();
+	}
+
+}
